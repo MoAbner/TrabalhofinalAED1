6ef2892a83501ccb10f78cfd7949982638c45300
MoAbner <153506543+MoAbner@users.noreply.github.com>
2024-11-29 19:38:55 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,25 +2,68 @@
 #include <time.h>
 #include <stdlib.h>
 
-void swap(int *x, int *y){ 
+void troca(int *x, int *y){ 
    int temp;
    temp=*x;
    *x=*y;
    *y=temp;
 }
-int particionamento(int abner[], int left, int rigth){
-    int mediana = (rigth - left) / 2;
-    int pivo1 = abner[mediana];    
-    int i = left;
 
-    for( int j = mediana + 1; j <= rigth; j++){
+int particionamento(int vetor[], int left, int rigth){
+    int mediana = (left + (rigth - left)/2);
+    troca(&vetor[mediana], &vetor[rigth]);
+    int pivo = vetor[rigth];
+    int i = left-1;
 
-        if(abner[j] <= pivo1){
+    for( int j = left; j < rigth; j++){
+
+        if(vetor[j] < pivo){
             i++;
-            swap(&j, &i);        
+            troca(&vetor[j], &vetor[i]);        
         }
+        troca(&vetor[i+1], &vetor[rigth]);    
     }
    
-    return(i);
+    return(i + 1);
 }
-// RELAÇÃO DE RECORRÊNCIA.
+
+void quickSort(int vetor[], int left, int rigth, clock_t *tempo){
+    clock_t inicio = clock();
+    if(left < rigth){
+        int indice_pivo = particionamento(vetor, left, rigth);
+        quickSort(vetor, left, indice_pivo - 1, tempo );
+        quickSort(vetor, indice_pivo + 1, rigth, tempo);
+    }
+    *tempo = clock() - inicio;
+}
+
+// Função que gera um vetor em ordem crescente
+void gerarVetorCrescente(int vetor[], int tamanhoVetor) {
+    tamanhoVetor = 9999900;
+    for (int i = 0; i < tamanhoVetor; i++) {
+        vetor[i] = i + 1;
+    }
+}
+int main() {
+
+    int elementos;
+
+    for(elementos = 100; elementos < 10000000; elementos *= 10){
+        int *vetor = (int*)malloc(elementos * sizeof(int));
+        
+        //a)
+        gerarVetorCrescente(vetor, elementos);
+
+        clock_t tempo;
+        printf("teste com %d elementos(Crescente): \n", elementos);
+
+        quickSort(vetor, 0, elementos - 1, &tempo );
+
+        printf("Tempo do QuickSort: %.2lf segundos\n", ((double)tempo/CLOCKS_PER_SEC) * 1000);
+    
+        free(vetor);
+        printf("sim 123");
+    }
+
+    return 0;
+}
